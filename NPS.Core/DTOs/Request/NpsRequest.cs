a44351b0fa3b922e887437bb7e5223d8eaf2f994
Matheus Gomes <math.s.gomes@gmail.com>
2024-11-10 22:55:25 -0300

    add nps request

---
+++ NPS.Core/DTOs/Request/NpsRequest.cs
@@ -0,0 +1,9 @@
+namespace NPS.Core.DTOs.Request;
+
+public class NpsRequest
+{
+    public int Score { get; set; }
+    public string CustomerName { get; set; }
+    public string Comment { get; set; }
+    public string Category { get; set; }
+}
