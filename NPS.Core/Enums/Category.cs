65cf318edc0422b7bc72eed856d2669759ff9f99
Matheus Gomes <math.s.gomes@gmail.com>
2024-12-08 16:45:02 -0300

    refact classify score

---
+++ NPS.Core/Enums/Category.cs
@@ -0,0 +1,8 @@
+namespace NPS.Core.Enums;
+
+public enum Category
+{
+    Promoter = 1,
+    Neutral = 2,
+    Detractor = 3,
+}
--- NPS.Core/Enums/Classificacao.cs
+++
@@ -1,8 +0,0 @@
-namespace NPS.Core.Enums;
-
-enum Classificacao
-{
-    Detrator = 1,
-    Neutro = 2,
-    Promotor = 3,
-}
