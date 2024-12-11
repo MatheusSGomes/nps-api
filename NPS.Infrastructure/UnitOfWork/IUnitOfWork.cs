0f46881ac3b7c61dd21cfbc80c80c0a8095d8f30
Matheus Gomes <math.s.gomes@gmail.com>
2024-12-10 22:29:43 -0300

    add unit of work with interface

--- NPS.Infrastructure/UnitOfWork/IUnitOfWork.cs
+++ NPS.Infrastructure/UnitOfWork/IUnitOfWork.cs
@@ -1,5 +1,9 @@
+using NPS.Core.Interfaces.Repositorios;
+
 namespace NPS.Infrastructure.UnitOfWork;
 
 public interface IUnitOfWork
 {
+    public INpsRepository NpsRepository { get; }
+    void Commit();
 }
--- NPS.Infrastructure/UnitOfWork/UnitOfWork.cs
+++ NPS.Infrastructure/UnitOfWork/UnitOfWork.cs
@@ -1,5 +1,24 @@
+using NPS.Core.Interfaces.Repositorios;
+using NPS.Infrastructure.Persistence;
+using NPS.Infrastructure.Repositories;
+
 namespace NPS.Infrastructure.UnitOfWork;
 
 public class UnitOfWork : IUnitOfWork
 {
+    private readonly NpsDbContext _context;
+    private readonly INpsRepository _npsRepository;
+
+    public UnitOfWork(NpsDbContext context, INpsRepository npsRepository)
+    {
+        _context = context;
+        _npsRepository = npsRepository;
+    }
+
+    public INpsRepository NpsRepository => _npsRepository ?? new NpsRepository(_context);
+
+    public async void Commit()
+    {
+        await _context.SaveChangesAsync();
+    }
 }
