c333635b2f45fef4c37927e6894156e4d43f044e
Matheus Gomes <math.s.gomes@gmail.com>
2024-12-16 21:37:04 -0300

    add di nps context, nps repository and unit of work

--- NPS.API/Program.cs
+++ NPS.API/Program.cs
@@ -1,6 +1,8 @@
-using System.Reflection;
+using Microsoft.EntityFrameworkCore;
 using NPS.Application.NpsCQ.Handlers;
-using NPS.Core.DTOs.Request;
+using NPS.Core.Interfaces.Repositorios;
+using NPS.Infrastructure.Persistence;
+using NPS.Infrastructure.Repositories;
 using NPS.Infrastructure.UnitOfWork;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,14 +10,19 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var configuration = builder.Configuration;
+
+builder.Services.AddDbContext<NpsDbContext>(options =>
+    options.UseSqlServer(configuration["ConnectionString"]));
 
 builder.Services.AddMediatR(cfg =>
     cfg.RegisterServicesFromAssembly(typeof(NpsCommandHandler).Assembly));
 
-builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped(typeof(INpsRepository), typeof(NpsRepository));
+builder.Services.AddScoped(typeof(IUnitOfWork), typeof(UnitOfWork));
 
 var app = builder.Build();
 
