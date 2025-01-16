1960675c38f9d0bc4c2bbdf6c634ae5f88ec07db
Matheus Gomes <math.s.gomes@gmail.com>
2025-01-15 23:40:38 -0300

    add query class to abstract connection with dapper

--- NPS.Infrastructure/Data/Queries/Query.cs
+++ NPS.Infrastructure/Data/Queries/Query.cs
@@ -1,6 +1,25 @@
+using Dapper;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
 namespace NPS.Infrastructure.Data.Queries;
 
 public class Query
 {
-    
+    private readonly IConfiguration _configuration;
+
+    public Query(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    protected async Task<T> QuerySingleAsync<T>(string sql, object parameters = null)
+    {
+        var connectionString = _configuration.GetConnectionString("DefaultConnection");
+        
+        await using (var connection = new SqlConnection(connectionString))
+        {
+            return await connection.QueryFirstOrDefaultAsync<T>(sql, param: parameters);
+        }
+    }
 }
