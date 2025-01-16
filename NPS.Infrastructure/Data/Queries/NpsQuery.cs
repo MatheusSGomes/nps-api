b7bc3df00c65a4a86f12129f92cc5d9ce53d869c
Matheus Gomes <math.s.gomes@gmail.com>
2025-01-15 23:40:46 -0300

    refact nps query

--- NPS.Infrastructure/Data/Queries/NpsQuery.cs
+++ NPS.Infrastructure/Data/Queries/NpsQuery.cs
@@ -1,6 +1,4 @@
 using System.Text;
-using Dapper;
-using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
 using NPS.Core.Nps.Filters;
 using NPS.Core.Nps.ViewModels;
@@ -9,20 +7,13 @@ namespace NPS.Infrastructure.Data.Queries;
 
 public class NpsQuery : Query, INpsQuery
 {
-    private readonly IConfiguration _configuration;
-
-    public NpsQuery(IConfiguration configuration)
+    public NpsQuery(IConfiguration configuration) : base(configuration)
     {
-        _configuration = configuration;
     }
 
     public async Task<int> GetNpsScore()
     {
-        var connectionString = _configuration.GetConnectionString("DefaultConnection");
-
-        await using (var connection = new SqlConnection(connectionString))
-        {
-            var sql = @"
+        const string sql = @"
                 SELECT
                     /* Porcentagem promotores */
                     (SELECT COUNT(Score) FROM [Nps] WHERE Score >= 9) * 100.0 /
@@ -31,47 +22,37 @@ public class NpsQuery : Query, INpsQuery
                     (SELECT COUNT(Score) FROM [Nps] WHERE Score <= 6) * 100.0 /
                     (SELECT COUNT(*) FROM [Nps]) AS Score;";
 
-            return await connection.QueryFirstOrDefaultAsync<int>(sql);
-        }
+        return await QuerySingleAsync<int>(sql);
     }
 
     public async Task<IEnumerable<NpsFullResponseViewModel>> GetNpsResponses(NpsFilters filters)
     {
-        var connectionString = _configuration.GetConnectionString("DefaultConnection");
+        StringBuilder sql = new();
 
-        await using (var connection = new SqlConnection(connectionString))
-        {
-            StringBuilder sql = new();
+        sql.AppendLine("SELECT Score, CustomerName, Comment FROM [Nps] WHERE 1=1");
 
-            sql.AppendLine("SELECT Score, CustomerName, Comment FROM [Nps] WHERE 1=1");
+        if (filters.Data != null)
+            sql.AppendLine("AND CAST(createdAt AS DATE) = @Data");
 
-            if (filters.Data != null)
-                sql.AppendLine("AND CAST(createdAt AS DATE) = @Data");
+        if (filters.CustomerName != null)
+            sql.AppendLine("AND CustomerName = @CustomerName");
 
-            if (filters.CustomerName != null)
-                sql.AppendLine("AND CustomerName = @CustomerName");
+        if (filters.Category != null)
+            sql.AppendLine("AND Category = @Category");
 
-            if (filters.Category != null)
-                sql.AppendLine("AND Category = @Category");
-
-            return await connection.QueryAsync<NpsFullResponseViewModel>(
-                sql: sql.ToString(),
-                param: new
-                {
-                    Data = filters.Data,
-                    CustomerName = filters.CustomerName,
-                    Category = filters.Category
-                });
-        }
+        return await QuerySingleAsync<List<NpsFullResponseViewModel>>(
+            sql: sql.ToString(),
+            parameters: new
+            {
+                Data = filters.Data,
+                CustomerName = filters.CustomerName,
+                Category = filters.Category
+            });
     }
 
     public async Task<NpsSummaryViewModel> GetNpsSummary()
     {
-        var connectionString = _configuration.GetConnectionString("DefaultConnection");
-
-        await using (var connection = new SqlConnection(connectionString))
-        {
-            var sql = @"SELECT 
+        var sql = @"SELECT 
                             /* Promoters */
                             CAST((SELECT COUNT(Score) FROM [Nps] WHERE Score >= 9) * 100.0 /
                                  (SELECT COUNT(*) FROM [Nps]) AS DECIMAL(10, 2)) AS Promoters,
@@ -87,7 +68,6 @@ public class NpsQuery : Query, INpsQuery
                                  (SELECT COUNT(Score) FROM [Nps] WHERE Score <= 6) * 100.0 /
                                  (SELECT COUNT(*) FROM [Nps]) AS Decimal(10, 2)) AS NpsScore;";
 
-            return await connection.QueryFirstOrDefaultAsync<NpsSummaryViewModel>(sql);
-        }
+        return await QuerySingleAsync<NpsSummaryViewModel>(sql);
     }
 }
