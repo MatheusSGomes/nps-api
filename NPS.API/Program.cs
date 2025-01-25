e4513b7012c0842b5548a1fb87359789b40d1058
Matheus Gomes <math.s.gomes@gmail.com>
2025-01-24 23:22:37 -0300

    add authentication jwt validations

--- NPS.API/Program.cs
+++ NPS.API/Program.cs
@@ -1,3 +1,6 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using NPS.API.Extensions;
 using NPS.API.Middleware;
 
@@ -7,6 +10,24 @@ builder.AddServices();
 builder.AddDependencyInjectionServices();
 builder.AddSqlServer();
 builder.AddMediatR();
+
+builder.Services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = "nps.com.br",
+            ValidAudience = "nps.com.br",
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("my_super_secret_key"))
+        };
+    });
+
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
@@ -20,6 +41,7 @@ if (app.Environment.IsDevelopment())
 app.UseMiddleware<UnitOfWorkMiddleware>();
 
 app.UseHttpsRedirection();
+app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
 app.Run();
