3200cc77c46cea2b37979e6aae67a28d57a3ff3b
Matheus Gomes <math.s.gomes@gmail.com>
2025-01-28 22:49:22 -0300

    test should include unique jti on generate token

--- NPS.Test/Services/AuthenticationServiceTest.cs
+++ NPS.Test/Services/AuthenticationServiceTest.cs
@@ -1,3 +1,4 @@
+using System.IdentityModel.Tokens.Jwt;
 using NPS.Application.Services;
 
 namespace NPS.Test.Services;
@@ -65,5 +66,41 @@ public class AuthenticationServiceTest
         var exception = Assert.Throws<ArgumentOutOfRangeException>(() =>
             authenticationService.GenerateJwtToken(username, inMemorySettings));
         Assert.Equal("Chave de autenticação inválida ou vazia, menor que 256 bits (32 bytes/caracteres)", exception.ParamName);
+    }
+
+    [Fact]
+    public void GenerateJwtToken_DeveIncluirClaimJtiUnico_QuandoTokenForGerado()
+    {
+        // Arrange
+        var authenticationService = new AuthenticationService();
+        var username = "username1";
+        var inMemorySettings = new Dictionary<string, string>()
+        {
+            { "Secret", "MINHA_CHAVE_SECRETA_A53D39BF-80CF-46F3-BFBB-7A3B69F33D17" },
+            { "Expires", "Authentication:Expires" },
+            { "Issuer", "Authentication:Issuer" },
+            { "Audience", "Authentication:Audience" }
+        };
+
+        // Act
+        var token = authenticationService.GenerateJwtToken(username, inMemorySettings);
+
+        // Assert
+        var handler = new JwtSecurityTokenHandler();
+        var jwtToken = handler.ReadJwtToken(token);
+        var jtiClaim = jwtToken.Claims.FirstOrDefault(c => c.Type == JwtRegisteredClaimNames.Jti);
+
+        // Verifica se o claim "jti" existe
+        Assert.NotNull(jtiClaim);
+
+        // Verifica se o valor do "jti" é único (geralmente, isso será verdadeiro se a geração do GUID estiver correta)
+        var jtiValue = jtiClaim?.Value;
+        Assert.False(string.IsNullOrEmpty(jtiValue));
+
+        // Testa se o valor de "jti" muda em chamadas subsequentes
+        var newToken = authenticationService.GenerateJwtToken(username, inMemorySettings);
+        var newJwtToken = handler.ReadJwtToken(newToken);
+        var newJtiClaim = newJwtToken.Claims.FirstOrDefault(c => c.Type == JwtRegisteredClaimNames.Jti);
+        Assert.NotEqual(jtiValue, newJtiClaim?.Value);
     }
 }
