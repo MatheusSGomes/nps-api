9bd27c35016eaeab72e4adeb4eb955a2ed5c3b04
Matheus Gomes <math.s.gomes@gmail.com>
2025-02-10 22:32:14 -0300

    add jwt token builder

---
+++ NPS.Application/Services/JwtTokenBuilder.cs
@@ -0,0 +1,83 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace NPS.Application.Services;
+
+public class JwtTokenBuilder
+{
+    private string _username = string.Empty;
+    private string _secret = string.Empty;
+    private string _issuer = string.Empty;
+    private string _audience = string.Empty;
+    private double _expires = 30;
+
+    public JwtTokenBuilder AddUsername(string username)
+    {
+        _username = username;
+        return this;
+    }
+
+    public JwtTokenBuilder AddSecret(string secret)
+    {
+        _secret = secret;
+        return this;
+    }
+
+    public JwtTokenBuilder AddIssuer(string issuer)
+    {
+        _issuer = issuer;
+        return this;
+    }
+
+    public JwtTokenBuilder AddAudience(string audience)
+    {
+        _audience = audience;
+        return this;
+    }
+
+    public JwtTokenBuilder AddExpires(double expires)
+    {
+        _expires = expires;
+        return this;
+    }
+
+    public string Build()
+    {
+        // Validações se tenho todos os atributos necessários
+        if (_secret is null)
+            throw new ArgumentNullException("Chave secreta inválida ou vazia");
+
+        if (_username is null)
+            throw new ArgumentNullException("Username deve ser informato");
+
+        if (_expires < 0)
+            throw new ArgumentNullException("Tempo de expiração não pode ser negativo");
+
+        byte[] encodedSecret = Encoding.UTF8.GetBytes(_secret);
+
+        // 1 byte = 8 bits
+        // 32 bytes = 256 bits
+        if (encodedSecret.Length < 32)
+            throw new ArgumentOutOfRangeException("Chave secreta menor que 256 bits (32 bytes/caracteres)");
+
+        var claims = new[]
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, _username),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+        };
+
+        var key = new SymmetricSecurityKey(encodedSecret);
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        var tokenJwt = new JwtSecurityToken(
+            issuer: _issuer,
+            audience: _audience,
+            claims: claims,
+            expires: DateTime.Now.AddMinutes(_expires),
+            signingCredentials: creds);
+
+        return new JwtSecurityTokenHandler().WriteToken(tokenJwt);
+    }
+}
