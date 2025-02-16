c39f195b68fd2d675272c64848169f6d1487a0ed
Matheus Gomes <math.s.gomes@gmail.com>
2025-02-16 16:00:38 -0300

    refact authentication service and interface remove generate jwt token method

--- NPS.Application/Services/AuthenticationService.cs
+++ NPS.Application/Services/AuthenticationService.cs
@@ -28,42 +28,4 @@ public class AuthenticationService : IAuthenticationService
 
         return token;
     }
-
-    public string GenerateJwtToken(string username, Dictionary<string, string> settings)
-    {
-        if (settings.IsNullOrEmpty())
-            throw new ArgumentNullException("Settings precisa ser configurado");
-
-        var secret = settings["Secret"];
-
-        if (secret is null)
-            throw new ArgumentNullException("Chave de autenticação inválida ou vazia");
-
-        byte[] encodedSecret = Encoding.UTF8.GetBytes(secret);
-
-        // 1 byte = 8 bits
-        // 32 bytes = 256 bits
-        if (encodedSecret.Length < 32)
-            throw new ArgumentOutOfRangeException("Chave de autenticação inválida ou vazia, menor que 256 bits (32 bytes/caracteres)");
-
-        var claims = new[]
-        {
-            new Claim(JwtRegisteredClaimNames.Sub, username),
-            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
-        };
-
-        var key = new SymmetricSecurityKey(encodedSecret);
-        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
-
-        double.TryParse(settings["Expires"], out double expiresAt);
-
-        var token = new JwtSecurityToken(
-            issuer: settings["Issuer"],
-            audience: settings["Audience"],
-            claims: claims,
-            expires: DateTime.Now.AddMinutes(expiresAt),
-            signingCredentials: creds);
-
-        return new JwtSecurityTokenHandler().WriteToken(token);
-    }
 }
--- NPS.Application/Services/IAuthenticationService.cs
+++ NPS.Application/Services/IAuthenticationService.cs
@@ -5,6 +5,5 @@ namespace NPS.Application.Services;
 public interface IAuthenticationService
 {
     string GenerateToken(IConfiguration configuration);
-    string GenerateJwtToken(string username, Dictionary<string, string> settings);
     AuthenticationService SetUsername(string userUsername);
 }
