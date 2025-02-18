6a551dbe1c94f6881196b10a75fb3784334979e2
Matheus Gomes <math.s.gomes@gmail.com>
2025-02-17 22:13:17 -0300

    test integration should saves nps correctly

--- NPS.Test.Integration/NpsRepositoryTests.cs
+++ NPS.Test.Integration/NpsRepositoryTests.cs
@@ -1,9 +1,46 @@
+using Microsoft.EntityFrameworkCore;
+using NPS.Core.Entities;
+using NPS.Infrastructure.Persistence;
+using NPS.Infrastructure.Repositories;
+using NPS.Infrastructure.UnitOfWork;
+
 namespace NPS.Test.Integration;
 
 public class NpsRepositoryTests
 {
-    [Fact]
-    public void Test1()
+    private readonly NpsDbContext _context;
+    private readonly NpsRepository _repository;
+    private readonly UnitOfWork _unitOfWork;
+
+    public NpsRepositoryTests()
     {
+        var options = new DbContextOptionsBuilder<NpsDbContext>()
+            .UseInMemoryDatabase("TestDb")
+            .Options;
+
+        _context = new NpsDbContext(options);
+        _repository = new NpsRepository(_context);
+        _unitOfWork = new UnitOfWork(_context, _repository);
+    }
+
+    [Fact]
+    public async Task SaveUserNps_DeveSalvarNpsCorreto()
+    {
+        // Arrange
+        var expectedScore = 10;
+        var expectedCustomerName = "Cliente Teste";
+        var expectedComment = "Comentário Teste";
+        var nps = Nps.CreateResponse(expectedScore, expectedCustomerName, expectedComment);
+
+        // Act
+        _unitOfWork.NpsRepository.Create(nps);
+        await _unitOfWork.Commit();
+
+        // Assert
+        var savedNps = await _context.Nps.FirstOrDefaultAsync(x => x.CustomerName == expectedCustomerName);
+        Assert.NotNull(savedNps);
+        Assert.Equal(expectedScore, savedNps.Score);
+        Assert.Equal(expectedCustomerName, savedNps.CustomerName);
+        Assert.Equal(expectedComment, savedNps.Comment);
     }
 }
