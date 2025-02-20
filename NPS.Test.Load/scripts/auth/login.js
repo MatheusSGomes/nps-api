8a3f7d8a97f5a25aea566a707c9e58f69816f551
Matheus Gomes <math.s.gomes@gmail.com>
2025-02-20 20:50:05 -0300

    test load auth login

---
+++ NPS.Test.Load/scripts/auth/login.js
@@ -0,0 +1,35 @@
+import http from 'k6/http';
+import {sleep, check} from 'k6';
+import {BASE_URL, MASTER_PASSWORD, MASTER_USERNAME} from "../../config/config.js";
+
+export const options = {
+    // interaction: 1,
+    stages: [
+      { duration: '30s', target: 20 },
+      { duration: '1m30s', target: 10 },
+      { duration: '20s', target: 0 },
+    ],
+}
+
+export default function() {
+    const credentials = {
+        username: MASTER_USERNAME,
+        password: MASTER_PASSWORD
+    };
+
+    const loginHeaders = {
+        headers: { 'Content-Type': 'application/json' },
+    };
+
+    const resLogin = http.post(`${BASE_URL}/api/Auth/login`, JSON.stringify(credentials), loginHeaders);
+
+    check(resLogin, {
+        'Status is 200': (r) => resLogin.status === 200,
+    });
+
+    check(resLogin, {
+        'Token exists': (r) => resLogin.json().token != null,
+    });
+
+    sleep(1);
+}
--- NPS.Test.Load/scripts/nps/score.js
+++ NPS.Test.Load/scripts/nps/score.js
@@ -1,36 +1,36 @@
 import http from 'k6/http';
-import { sleep, check } from 'k6';
+import {sleep, check} from 'k6';
 import {BASE_URL} from "../../config/config.js";
-import { generateJwtToken } from "../../common/jwt_generate.js";
+import {generateJwtToken} from "../../common/jwt_generate.js";
 
 export const options = {
-  // vus: 1,
-  // duration: '2s',
-  // interaction: 1
+    // vus: 1,
+    // duration: '2s',
+    interaction: 1,
 
-  stages: [
-    { duration: '30s', target: 20 },
-    { duration: '1m30s', target: 10 },
-    { duration: '20s', target: 0 },
-  ],
+    // stages: [
+    //   { duration: '30s', target: 20 },
+    //   { duration: '1m30s', target: 10 },
+    //   { duration: '20s', target: 0 },
+    // ],
 };
 
-export default function() {
-  const scoreHeaders = {
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer ' + generateJwtToken()
-    },
-  };
+export default function () {
+    const scoreHeaders = {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + generateJwtToken()
+        },
+    };
 
-  let res = http.get(`${BASE_URL}/v1/Nps/Score`, scoreHeaders);
-  const jsonResponse = res.json();
+    let res = http.get(`${BASE_URL}/v1/Nps/Score`, scoreHeaders);
+    const jsonResponse = res.json();
 
-  check(res, { "Status is 200": (res) => res.status === 200 });
+    check(res, {"Status is 200": (res) => res.status === 200});
 
-  check(res, {
-    'Body contains score key': (r) => jsonResponse.score != null,
-  });
+    check(res, {
+        'Body contains score key': (r) => jsonResponse.score != null,
+    });
 
-  sleep(1);
+    sleep(1);
 }
