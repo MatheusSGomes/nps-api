47ef178c0981f5c6a358dfee739eb5b9ec0f860f
Matheus Gomes <math.s.gomes@gmail.com>
2025-02-19 21:24:15 -0300

    add checks for score load tests

--- NPS.Test.Load/scripts/score/script.js
+++ NPS.Test.Load/scripts/score/script.js
@@ -1,18 +1,32 @@
 import http from 'k6/http';
 import { sleep, check } from 'k6';
+import {BASE_URL} from "../config/config";
 
 export const options = {
-  vus: 10,
-  duration: '30s',
-  // stages: [
-  //   { duration: '30s', target: 20 },
-  //   { duration: '1m30s', target: 10 },
-  //   { duration: '20s', target: 0 },
-  // ],
+  // vus: 10,
+  // duration: '30s',
+  stages: [
+    { duration: '30s', target: 20 },
+    { duration: '1m30s', target: 10 },
+    { duration: '20s', target: 0 },
+  ],
 };
 
 export default function() {
-  let res = http.get('https://quickpizza.grafana.com');
+  let res = http.get(`${BASE_URL}/v1/Nps/Score`);
+
   check(res, { "status is 200": (res) => res.status === 200 });
+
+  check(response, {
+    'Corpo contém conteúdo esperado': (r) => r.body.includes('score'),
+  });
+
+  check(response, {
+    'Contém a chave score': (r) => {
+      const jsonBody = JSON.parse(r.body);
+      return jsonBody.score != null;
+    },
+  });
+
   sleep(1);
 }
