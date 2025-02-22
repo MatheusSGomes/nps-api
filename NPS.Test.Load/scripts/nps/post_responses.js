bf82b76a59b3a6cd60f0b8a04fe858d7b690907a
Matheus Gomes <math.s.gomes@gmail.com>
2025-02-22 12:47:58 -0300

    test load add tags, thresholds and checks

--- NPS.Test.Load/scripts/nps/post_responses.js
+++ NPS.Test.Load/scripts/nps/post_responses.js
@@ -5,6 +5,14 @@ import { getRandomNumber } from "../../common/random_number_generate.js";
 
 export const options = {
     // interaction: 1,
+    tags: {
+        test_name: "Teste_Carga_Post_Responses",
+        environment: "TST"
+    },
+    thresholds: {
+        'http_req_duration{status:200}': ['p(95)<500'], // p95 abaixo de 500ms
+        http_req_duration: ['p(90)<500'], // 90% das requisições devem ser < 500ms
+    },
     stages: [
         { duration: '30s', target: 20 },
         { duration: '1m30s', target: 10 },
@@ -26,7 +34,8 @@ export default function() {
     const response = http.post(`${BASE_URL}/v1/Nps/Responses`, JSON.stringify(scoreResponse), headers);
 
     check(response, {
-        "Status is 200": (r) => r.status === 200
+        "Status is 200": (r) => r.status === 200,
+        "Response timing < 500ms": (r) => r.timings.duration < 500
     });
 
     sleep(0.5); // Simula uma pausa de 500ms entre as requisições
