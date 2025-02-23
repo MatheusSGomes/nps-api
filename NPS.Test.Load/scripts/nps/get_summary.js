38574574c0f497ed42a50cc4a91cf4efeed1e499
Matheus Gomes <math.s.gomes@gmail.com>
2025-02-23 19:57:50 -0300

    test load add tag and thresholds on get summary

--- NPS.Test.Load/scripts/nps/get_summary.js
+++ NPS.Test.Load/scripts/nps/get_summary.js
@@ -5,6 +5,14 @@ import {generateJwtToken} from "../../common/jwt_generate.js";
 
 export const options = {
     // interaction: 1,
+    tags: {
+        test_name: "Teste_Carga_Get_Summary",
+        environment: "TST"
+    },
+    thresholds: {
+        'http_req_duration{status:200}': ['p(95)<500'], // p95 abaixo de 500ms
+        http_req_duration: ['p(90)<500'], // 90% das requisições devem ser < 500ms
+    },
     stages: [
       { duration: '30s', target: 20 },
       { duration: '1m30s', target: 10 },
